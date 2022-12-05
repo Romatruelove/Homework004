8a7cb0214c7b5e62d963cb81b3f8140885f0a664
Roman Filimonov <romatruelove@yandex.ru>
2022-12-05 15:56:00 +0300

    final code

---
+++ Program25/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число А:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        void ToDegree(int a, int b)
+        {
+            int result = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                result = result * a;
+            }
+            Console.WriteLine($"{a}^{b}={result}");
+        }
+        ToDegree(a, b);
+    }
+}
---
+++ Program27/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        static int digSum(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                sum = sum + n % 10;
+                n = n / 10;
+            }
+            return sum;
+        }
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма всех цифр числа {n} = {digSum(n)}");
+        }
+        Main();
+    }
+}
